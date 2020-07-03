e9497e96bffc058fac24fd027639c3a59e5fae59
mirror <mirrory@ciandt.com>
2020-07-03 16:18:05 +0800

    [Mirror] Project initialization.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h2>JSON和YML在线转换</h2>
+    <div className="button-group">
+      <button onClick={this.onConfigClick}>Config</button>
+      <button>Convert</button>
+      <button>Save</button>
+      <button>Run</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    padding: 10px;
+  }
+  h2 {
+    flex: 1;
+  }
+</style>
